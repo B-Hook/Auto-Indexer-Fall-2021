9f0d91e9d008cf9a946d8e0693d035e0076415ec
Braiden Hook <bghook@smu.edu>
2021-09-22 08:47:26 -0500

    adding DSString

---
+++ DSString.cpp
@@ -0,0 +1,133 @@
+//
+// Created by Braiden Hook on 9/22/21.
+//
+
+#include "DSString.h"
+#include <iostream>
+
+DSString::DSString() {
+    data = nullptr;
+}
+
+DSString::DSString(const char *cStr) {
+    this->data = new char[strlen(cStr) + 1]; // Creates a new cstring based on the length of the passed cstring
+    strcpy(this->data, cStr); // Copies the passed cstring to the object data
+}
+
+DSString::DSString(const DSString &obj) {
+    // Creates a new cstring based on the length of the data of the passed obj
+    this->data = new char[strlen(obj.data) + 1];
+    strcpy(this->data, obj.data); // Copies the data from the passed obj to the new object data
+}
+
+DSString::~DSString() {
+
+    if (this->data != nullptr){
+        delete[] this->data; //deleting the cstring
+    }
+
+}
+
+DSString &DSString::operator=(const char *copy) {
+    // If the data does not equal the copy
+    if (this->data != copy) {
+        delete[] this->data; // Deletes the cstring
+        this->data = new char[strlen(copy) + 1]; // New cstring
+        strcpy(this->data, copy); // Copies the passed cstring to data
+    }
+    return *this; // Returns the object
+}
+
+DSString &DSString::operator=(const DSString &copy) {
+    // If the data does not equal the copy's data
+    if (this->data != copy.data) {
+        delete[] this->data; // Deletes the cstring
+        this->data = new char[strlen(copy.data) + 1]; // New cstring
+        strcpy(this->data, copy.data); // Copies the passed data's cstring to this object's data
+    }
+    return *this; // Returns this object
+}
+
+DSString DSString::operator+(const DSString &a) {
+    // Creates a new cstring adding the length of the data and the passed data + 1 for the 0/
+    char newStr [strlen(this->data) + strlen(a.data) + 1];
+    strcpy(newStr, this->data); // Copies this objects data in the new str
+    strcat(newStr, a.data); // Adds the passed data
+
+    return newStr; // Returns the new obj
+}
+
+bool DSString::operator==(const char *c) const{
+    // Compares the data to the passed cstring
+    int a = strcmp(this->data, c);
+    // If they equal return true, if not then return false
+    if (a == 0)
+        return true;
+    else
+        return false;
+}
+
+bool DSString::operator==(const DSString &c) const{
+    // Compares the data to the passed object's data
+    int a = strcmp(this->data, c.data);
+    // If they equal return true, if not then return false
+    if (a == 0)
+        return true;
+    else
+        return false;
+}
+
+bool DSString::operator>(const DSString &c) const{
+    // Compares the data to the passed object's data
+    int a = strcmp(this->data, c.data);
+    // If a is greater than 0 then true, if not false
+    if (a > 0)
+        return true;
+    else
+        return false;
+}
+
+bool DSString::operator<(const DSString &c) const{
+    // Compares the data to the passed object's data
+    int a = strcmp(this->data, c.data);
+    // If a is less than 0 then true, if not false
+    if (a < 0)
+        return true;
+    else
+        return false;
+}
+
+
+bool DSString::operator>(const char *c) const{
+    // Compares the data to the passed cstring
+    int a = strcmp(this->data, c);
+    // If a is greater than 0 then true, if not false
+    if (a > 0)
+        return true;
+    else
+        return false;
+}
+
+char &DSString::operator[](const int index) {
+    return this->data[index]; // Returns the char at the specfic passed index
+}
+
+int DSString::getLength() {
+    return strlen(this->data); // Returns length of the cstring
+}
+
+DSString DSString::substring(int start, int numChars) {
+    DSString newObj; // Creates new object
+    newObj.data = new char [numChars + 1]; // Makes the cstring the length of num of characters wanting to read in
+    strncpy(newObj.data, this->data + start, numChars); // Copies the data from this object into the new one
+    newObj.data[numChars] = '\0'; // Puts a null char at the end of the cstring to terminate it
+    return newObj; // Returns the new object
+}
+
+char *DSString::c_str() {
+    return this->data; // Returns the cstring
+}
+
+std::ostream &operator<<(std::ostream &oFile, const DSString &objPrint) {
+    return oFile << objPrint.data; // Prints the object to the output file
+}
--- sample_tests.cpp
+++ sample_tests.cpp
@@ -14,8 +14,123 @@
  */
 
 #include "catch.hpp"
+#include "DSString.h"
 
 TEST_CASE("Testing Tests", "[multiplication]") {
     REQUIRE((2 * 2) == 4);
     REQUIRE((1 * 5) == 5);
+}
+
+
+
+TEST_CASE("Testing DSString")
+{
+    DSString a = "1";
+    DSString b = "Ten Char10";
+    DSString c = "Multiple words/Actual Tweet";
+    DSString d = "Ten Char10";
+    DSString e = "UPPERCASE";
+    DSString f = "uppercase";
+    DSString g = "";
+    DSString h = "\n";
+    DSString i = "                    ";
+    DSString j = "aaaa";
+    DSString k = "bbbb";
+    char char1 = 'y';
+    char char2 = 'n';
+    char char3 = ' ';
+    char char4 = '\n';
+    char char5 = e[0];
+    char char6 = 'y';
+    char char7 = 'Y';
+    char char9 = '1';
+    char char10 = '9';
+
+
+    SECTION("Equality operators DSString"){
+        REQUIRE(a == DSString("1"));
+        REQUIRE(b == d);
+        REQUIRE(c == "Multiple words/Actual Tweet");
+        REQUIRE(!(e == f));
+    }
+    SECTION("Equality operators Char"){
+        REQUIRE(char1 == 'y');
+        REQUIRE(char1 == char6);
+        REQUIRE(char2 == b[2]);
+        REQUIRE(char3 == i[1]);
+        REQUIRE(char4 == h[0]);
+        REQUIRE(!(char5 == f[0]));
+    }
+    SECTION("Assignment operators DSString"){
+        DSString str;
+        str = "1";
+        REQUIRE(str == a);
+        str = DSString("Ten Char10");
+        REQUIRE(str == b);
+        str = "";
+        REQUIRE(str == g);
+        str = DSString("\n");
+        REQUIRE(str == h);
+    }
+    SECTION("Assignment operators Char"){
+        char char8;
+        char8 = '1';
+        REQUIRE(char8 == a[0]);
+        char8 = char('a');
+        REQUIRE(char8 == j[0]);
+        char8 = ' ';
+        REQUIRE(char8 == i[2]);
+        char8 = 'a';
+        REQUIRE(char8 == 'a');
+        char8 = '\n';
+        REQUIRE(char8 == char4);
+    }
+    SECTION("Addition operator DSString"){
+        REQUIRE(DSString("1Ten Char10") == a + b);
+        REQUIRE(g + g == "");
+        REQUIRE(g + h == DSString("\n"));
+        REQUIRE(a + b + c == "1Ten Char10Multiple words/Actual Tweet");
+    }
+
+    SECTION("Greater than operator DSString"){
+        REQUIRE(b > a);
+        REQUIRE(f > e);
+        REQUIRE(k > j);
+        REQUIRE(c > g);
+        REQUIRE(b > g);
+    }
+    SECTION("Greater than operator DSString"){
+        REQUIRE(a < b);
+        REQUIRE(e < f);
+        REQUIRE(j < k);
+        REQUIRE(g < c);
+        REQUIRE(g < b);
+    }
+    SECTION("Greater than operator Char"){
+        REQUIRE(char1 > char2);
+        REQUIRE(char6 > char7);
+        REQUIRE(char2 > char7);
+        REQUIRE(char10 > char9);
+    }
+    SECTION("[] Operator"){
+        REQUIRE(b[1] == 'e');
+        REQUIRE(c[8] == ' ');
+        REQUIRE(g[0] == 0);
+    }
+    SECTION("getLength function"){
+        REQUIRE(d.getLength() == 10);
+        REQUIRE(g.getLength() == 0);
+        REQUIRE(i.getLength() == 20);
+        REQUIRE(e.getLength() == 9);
+    }
+    SECTION("Substring function") {
+        REQUIRE(a.substring(0, 1) == "1");
+        REQUIRE(f.substring(0, 5) == "upper");
+        REQUIRE(f.substring(1, 6) == "pperca");
+    }
+    SECTION("c_str function"){
+        REQUIRE(strcmp(b.c_str(), "Ten Char10") == 0);
+        REQUIRE(strcmp(d.c_str(), b.c_str()) == 0);
+        REQUIRE(strcmp(g.c_str(), "") == 0);
+    }
 }
