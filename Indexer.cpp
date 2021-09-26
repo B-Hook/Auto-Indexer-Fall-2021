1c054c128adb6cb2543d61ece3689c3e203faab8
Braiden Hook <bghook@smu.edu>
2021-09-25 23:56:30 -0500

    Progress on indexer

--- Indexer.cpp
+++ Indexer.cpp
@@ -14,8 +14,8 @@ void Indexer::readInFile(char *input) {
 
     char extendedWord[85];
     char extendedNested[85];
-    int charCountW = 0;
-    int charCountN = 0;
+    int countW = 0;
+    int countN = 0;
     char char1[85];
     char pageNum[85];
     DSString pageNumObj;
@@ -34,6 +34,71 @@ void Indexer::readInFile(char *input) {
             pageNumObj = pageNum;
             pageNumV.push_back(pageNumObj);
         }
+        else{
+            for (int i = 0; i < strlen(char1); i++){
+
+                if (char1[i] == '['){
+                    char word1[85];
+                    i++;
+                    for (;i < strlen(char1); i++){
+                        if ((char1[i] != '[') && (char1[i] != ']')){
+                            word1[countW] = char1[i];
+                            countW++;
+                            //i++;
+                        }
+
+                        else if (char1[i] == ']'){
+                            word1[countW] = '\0';
+                            cout << word1 << endl;
+                            memset(word1, 0, strlen(char1));
+                            //obj1(word1, pageNumObj)
+                            countW = 0;
+                            //i++;
+                            break;
+                        }
+                        else if (char1[i] == '['){
+                            char nested1[85];
+                            i++;
+                            for (; i < strlen(char1); i++){
+                                if(char1[i] != ']'){
+                                    nested1[countN] = char1[i];
+                                    word1[countW] = char1[i];
+                                    countN++;
+                                    countW++;
+                                    //i++;
+                                    //j++;
+                                }
+                                else if (char1[i] == ']'){
+                                    //obj1(nested1, pageNumObj)
+                                    nested1[countN] = '\0';
+                                    cout << nested1 << endl;
+                                    memset(nested1, 0, strlen(char1));
+                                    countN = 0;
+                                    //i++;
+                                    //j++;
+                                    break;
+                                }
+
+                            }
+
+                        }
+
+                    }
+
+                }
+
+
+
+            }
+
+
+
+
+        }
+
+
+
+
     }
     for (int i = 0; i < pageNumV.getSize(); i++)
         cout << pageNumV.at(i) << endl;
